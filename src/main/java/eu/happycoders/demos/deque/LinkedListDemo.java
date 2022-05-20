e9e77ce32d129b9136cdcc7f82e7649ea776e227
Sven Woltmann <sven@happycoders.eu>
2022-05-20 14:19:30 +0200

    Add LinkedListDemo

---
+++ src/main/java/eu/happycoders/demos/deque/LinkedListDemo.java
@@ -0,0 +1,47 @@
+package eu.happycoders.demos.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Demo for Java's {@link ArrayDeque} implementation.
+ *
+ * @author <a href="sven@happycoders.eu">Sven Woltmann</a>
+ */
+public class LinkedListDemo {
+  public static void main(String[] args) {
+    Deque<Integer> deque = new LinkedList<>();
+
+    for (int i = 0; i < 8; i++) {
+      int element = ThreadLocalRandom.current().nextInt(10, 100);
+      if (ThreadLocalRandom.current().nextBoolean()) {
+        deque.offerFirst(element);
+        System.out.println("deque.offerFirst(" + element + ") --> deque = " + deque);
+      } else {
+        deque.offerLast(element);
+        System.out.println("deque.offerLast(" + element + ")  --> deque = " + deque);
+      }
+    }
+
+    System.out.println();
+    System.out.println("deque.isEmpty()   = " + deque.isEmpty());
+    System.out.println("deque.peekFirst() = " + deque.peekFirst());
+    System.out.println("deque.peekLast()  = " + deque.peekLast());
+    System.out.println();
+
+    while (!deque.isEmpty()) {
+      if (ThreadLocalRandom.current().nextBoolean()) {
+        System.out.println("deque.pollFirst() = " + deque.pollFirst() + " --> deque = " + deque);
+      } else {
+        System.out.println("deque.pollLast()  = " + deque.pollLast() + " --> deque = " + deque);
+      }
+    }
+
+    System.out.println();
+    System.out.println("deque.isEmpty()   = " + deque.isEmpty());
+    System.out.println("deque.peekFirst() = " + deque.peekFirst());
+    System.out.println("deque.peekLast()  = " + deque.peekLast());
+  }
+}
